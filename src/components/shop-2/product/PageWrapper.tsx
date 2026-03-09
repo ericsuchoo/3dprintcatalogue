763ca4511171ded415fb22f0a693e3b05193441f
ericsuchoo <70877515+ericsuchoo@users.noreply.github.com>
2026-03-08 23:59:11 -0600

    feat: dynamic product tag labels from etiquetas relation nuevo cambio 15

--- src/components/shop-2/product/PageWrapper.tsx
+++ src/components/shop-2/product/PageWrapper.tsx
@@ -1,32 +1,21 @@
-import ContextWrapper from '../../ContextWrapper';
-import InnerPageWrapper from '../../InnnerPageWrapper';
-import type { ClientConfig } from '@thebcms/client';
-import { Main } from './Main';
-import type { ProductLite } from '../../../utils/product';
-import type { ProductEntryMetaItem } from '../../../../bcms/types/ts';
+import React from "react";
+import ContextWrapper from "../../ContextWrapper";
+import InnerPageWrapper from "../../InnnerPageWrapper";
+import { Main } from "../../shop/products/Main";
+import type { ShopPageDataD1 } from "../../../types/shop-d1";
 
 interface Props {
-    data: {
-        meta: ProductEntryMetaItem;
-        otherProducts: ProductLite[];
-    };
-    bcms: ClientConfig;
+  data: ShopPageDataD1;
 }
 
-const ProductPageWrapper: React.FC<Props> = ({ data, bcms }) => {
-    return (
-        <ContextWrapper>
-            <InnerPageWrapper bcms={bcms}>
-                <div className="container pb-14 md:pb-20 lg:pb-[136px]">
-                    <Main
-                        meta={data.meta}
-                        otherProducts={data.otherProducts}
-                        bcms={bcms}
-                    />
-                </div>
-            </InnerPageWrapper>
-        </ContextWrapper>
-    );
+const ProductsPageWrapper: React.FC<Props> = ({ data }) => {
+  return (
+    <ContextWrapper>
+      <InnerPageWrapper>
+        <Main data={data} favoritesOnly />
+      </InnerPageWrapper>
+    </ContextWrapper>
+  );
 };
 
-export default ProductPageWrapper;
+export default ProductsPageWrapper;
