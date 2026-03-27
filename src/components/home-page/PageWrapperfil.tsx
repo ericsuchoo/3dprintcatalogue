07a188f298549e6a562e31a06a7ad0f02e4cf536
ericsuchoo <70877515+ericsuchoo@users.noreply.github.com>
2026-03-26 22:27:18 -0600

    fix: cargar img_personaje desde personajes_imagenes con fallback seguro 3

--- src/components/home-page/PageWrapperfil.tsx
+++ src/components/home-page/PageWrapperfil.tsx
@@ -188,7 +188,7 @@ const NewPageWrapper: React.FC<Props> = ({
   />
 )}
 
-          <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-8 mt-16">
             {pageCategories.length > 0 ? (
               <CategoriesMini data={pageCategories} />
             ) : (
